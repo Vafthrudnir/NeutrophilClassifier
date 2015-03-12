26a4108b58e9bcefbb3fe0faae74ba1b41af9fe9
Tamas Levai <levai.tamas@t-online.hu>
2015-03-12 23:01:24 +0100

    Update img_proc_test.py; set uniform line ending ; add test.tif to .gitignore

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 
 #35698_ARHGAP25_Fbr_1_1
-
 img = cv2.imread("35698_ARHGAP25_Fbr_1_1.tif", cv2.IMREAD_COLOR)
 
-cv2.imshow("Image2", img)
+cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+cv2.imshow("image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
