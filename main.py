93d6189ce6c37e197db2292404f0be95bb9cc91b
Dave <szd0517@gmail.com>
2015-03-12 11:37:10 +0100

    git proba

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 
 #35698_ARHGAP25_Fbr_1_1
 
-img = cv2.imread("imgs/35698_ARHGAP25_Fbr_1_1.tif", cv2.IMREAD_COLOR)
+img = cv2.imread("35698_ARHGAP25_Fbr_1_1.tif", cv2.IMREAD_COLOR)
 
-cv2.imshow("Image", img)
+cv2.imshow("Image2", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
